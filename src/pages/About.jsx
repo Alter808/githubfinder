c9dcdf09eb7cadc974b6574ea772aee0d47c0452
carlos <altermotions@gmail.com>
2022-04-04 15:59:45 -0400

    About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,8 @@ function About() {
     <div>
       <h1 className="text-6xl mb-4">Github Finder</h1>
       <p className="mb-4 text-2xl font-light">
-        A React app to search GitHub profiles and see profile details.
+        A React app to search GitHub profiles and see profile details. Developed
+        by Carlos Ortiz as a code challenge
       </p>
       <p className="text-lg text-gray-400">
         Version <span className="text-white">1.0.0</span>
